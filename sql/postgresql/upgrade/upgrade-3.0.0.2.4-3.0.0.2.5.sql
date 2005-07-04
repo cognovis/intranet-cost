b78086cc2a27e05f453d8b71d258de3f99cbc2e1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-07-04 11:53:47 +0000

    - removed again

--- sql/postgresql/upgrade/upgrade-3.0.0.2.4-3.0.0.2.5.sql
+++
@@ -1,9 +0,0 @@
-
-alter table im_projects alter   cost_quotes_cache               set default 0;
-alter table im_projects alter   cost_invoices_cache             set default 0;
-alter table im_projects alter   cost_timesheet_planned_cache    set default 0;
-
-alter table im_projects alter   cost_purchase_orders_cache      set default 0;
-alter table im_projects alter   cost_bills_cache                set default 0;
-alter table im_projects alter   cost_timesheet_logged_cache     set default 0;
-
