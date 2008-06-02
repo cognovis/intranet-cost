f9ad06c283025250b7f263acf53714a0d0dd85ad
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-06-02 15:09:07 +0000

    - added trigger change to "after update or delete"

--- sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.2.0-3.3.1.2.1.sql
@@ -134,3 +134,29 @@ select  category_id as cost_type_id,
 from    im_categories
 where   category_type = 'Intranet Cost Type';
 
+
+
+
+-- drop constraint if exists...
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	select count(*) into v_count from pg_trigger
+	where lower(tgname) = ''im_payments_audit_tr'';
+	IF v_count = 0 THEN return 0; END IF;
+
+	drop trigger im_payments_audit_tr on im_payments;
+
+	return v_count;
+end;' language 'plpgsql';
+SELECT inline_0();
+DROP FUNCTION inline_0();
+
+
+create trigger im_payments_audit_tr
+after update or delete
+on im_payments
+for each row execute procedure im_payments_audit_tr();
+
